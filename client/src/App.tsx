a66a16ad2d98cfba0a8f7a84d470f86205d99f19
Willoandru <wilsonandrescriollo@gmail.com>
2026-01-06 11:53:37 -0500

    Escabilidad estilos y logica handleSearch

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -26,9 +26,20 @@ function App() {
   const [loading, setLoading] = useState<boolean>(false);
   const [sortBy, setSortBy] = useState<string>("price_asc");
   const [error, setError] = useState<string>("");
-  const [selectedMarkets, setSelectedMarkets] = useState(() => new Set());
 
-  const toggleMarket = (id: string) => {
+  const [selectedMarkets, setSelectedMarkets] = useState(() => new Set());
+  const SUPER_MARKETS = [
+    { img: "/exito.png" },
+    // { img: "/d1.png" },
+    // { img: "/jumbo.png" },
+    // { img: "/olimpica.png" },
+    // { img: "/alkosto.png" },
+  ];
+
+  // Maneja la logica de seleccion de supermercados
+  const toggleMarket = (id: number) => {
+    if (error) setError("");
+
     setSelectedMarkets((prev) => {
       const next = new Set(prev);
       next.has(id) ? next.delete(id) : next.add(id);
@@ -36,18 +47,26 @@ function App() {
     });
   };
 
+  // Busqueda
   const handleSearch = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     if (!query.trim()) return;
+    if (!selectedMarkets.size) {
+      setError("Seleccione por lo menos un supermercado");
+      return;
+    }
     setData(null);
     setLoading(true);
 
     try {
-      const response = await axios.get<ScrapeResult>(`${API_URL}/search`, {
-        params: { query, sortBy },
-      });
-      setData(response.data);
+      let response = null;
+      if (selectedMarkets.has(0)) {
+        response = await axios.get<ScrapeResult>(`${API_URL}/search`, {
+          params: { query, sortBy },
+        });
+      }
+      if (response) setData(response.data);
     } catch (error) {
       setError(String(error));
     } finally {
@@ -55,41 +74,36 @@ function App() {
     }
   };
 
-  const SUPER_MARKETS = [
-    { id: "exito", img: "/exito.png" },
-    { id: "d1", img: "/d1.png" },
-    { id: "jumbo", img: "/jumbo.png" },
-    { id: "olimpica", img: "/olimpica.png" },
-    { id: "alkosto", img: "/alkosto.png" },
-  ];
-
   return (
-    <div className="w-full bg-gray-200 dark:bg-gray-800 min-h-screen flex items-center justify-center flex-col p-4 gap-6 transition-colors">
+    <main className="w-full bg-gray-200 dark:bg-gray-800 min-h-screen flex items-center justify-center flex-col p-4 gap-6 transition-colors">
       <Theme />
       <section className="flex gap-4 text-3xl font-bold items-center">
         <img src="/icon.png" className="w-15" alt="icon" />
         <h1>PriceCompare</h1>
       </section>
       <div className="flex gap-3">
-        {SUPER_MARKETS.map((item) => {
-          const isActive = selectedMarkets.has(item.id);
+        {SUPER_MARKETS.map((item, i) => {
+          const isActive = selectedMarkets.has(i);
           return (
             <button
-              key={item.id}
-              onClick={() => toggleMarket(item.id)}
+              key={i}
+              onClick={() => toggleMarket(i)}
               className={`rounded-xl p-2 ${
                 isActive
-                  ? "bg-gray-900 ring-2 ring-gray-900 dark:bg-gray-100 dark:ring-gray-100"
+                  ? "bg-emerald-400"
                   : "bg-white hover:bg-gray-100 dark:bg-gray-900 dark:hover:bg-gray-700"
               }`}
-              aria-pressed={isActive}
             >
               <img src={item.img} className="w-12 rounded-xl" />
             </button>
           );
         })}
       </div>
-
+      {error && (
+        <section className="rounded border px-4 py-3 font-semibold bg-red-100 text-red-700 dark:bg-red-900 dark:text-red-200">
+          {error}
+        </section>
+      )}
       <form
         onSubmit={handleSearch}
         className="flex border rounded overflow-hidden"
@@ -98,27 +112,24 @@ function App() {
           value={query}
           onChange={(e) => setQuery(e.target.value)}
           type="text"
-          className="rounded px-4 focus:outline-0"
+          className="w-70 rounded px-4 focus:outline-0"
+          placeholder="Ingrese el producto a buscar"
         />
         <button className="p-3 bg-emerald-400 border-l hover:bg-emerald-300">
           <FaSearch />
         </button>
       </form>
+      {selectedMarkets.has(0) && <Exito />}
       {loading && (
         <section className="font-semibold text-gray-600 dark:text-gray-400">
           Buscando productos para "{query}"
-        </section>
-      )}
-      {error && (
-        <section className="rounded border px-4 py-3 font-semibold bg-red-100 text-red-700 dark:bg-red-900 dark:text-red-200">
-          {error}
         </section>
       )}
       {data && (
         <div className="bg-white dark:bg-gray-900 p-4 rounded text-xs">
           <section className="flex gap-2 items-center mb-2">
             <img className="h-10" src="/exito.png" alt="exito" />
-            <h2>{data.totalCount} resultados</h2>
+            <h6>{data.totalCount} resultados</h6>
           </section>
           <table className="min-w-full bg-white dark:bg-gray-800 rounded overflow-hidden">
             <thead className="bg-gray-200 dark:bg-gray-700">
@@ -162,7 +173,7 @@ function App() {
           </table>
         </div>
       )}
-    </div>
+    </main>
   );
 }
 
--- client/src/components/Exito.tsx
+++ client/src/components/Exito.tsx
@@ -1,3 +1,7 @@
 export default function Exito() {
-  return <div className=""></div>;
+  return (
+    <div className="">
+      <h4>Seleccione el numero de paginas</h4>
+    </div>
+  );
 }
