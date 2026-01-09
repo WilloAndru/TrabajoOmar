25e7dfb87c26a9a02e86a49133b258c9f5c67139
Willoandru <wilsonandrescriollo@gmail.com>
2026-01-09 16:58:07 -0500

    Ya se pueden extraer todos los productos por paginas de Jumbo

--- api/controllers/jumbo.controller.js
+++ api/controllers/jumbo.controller.js
@@ -1,24 +1,4 @@
-import {
-  getTotalCountJumbo,
-  getProductsJumbo,
-} from "../services/jumbo.service.js";
-
-export const searchTotalCountJumbo = async (req, res) => {
-  const { query } = req.query;
-
-  if (!query) {
-    return res.status(400).json({ error: "No hay consulta" });
-  }
-
-  try {
-    const response = await getTotalCountJumbo(query);
-    return res.json(response);
-  } catch (err) {
-    return res.status(500).json({
-      error: "Error en el servicio de Jumbo",
-    });
-  }
-};
+import { getProductsJumbo } from "../services/jumbo.service.js";
 
 export const searchProductsJumbo = async (req, res) => {
   const { query } = req.query;
--- api/services/jumbo.service.js
+++ api/services/jumbo.service.js
@@ -1,87 +1,110 @@
 import { fetch } from "undici";
 
-export const getTotalCountJumbo = async (query) => {
-  const url = `https://www.jumbocolombia.com/${encodeURIComponent(
-    query
-  )}/s?_q=${encodeURIComponent(
-    query
-  )}&map=ft&__pickRuntime=appsEtag,blocks,blocksTree,components,contentMap,extensions,messages,page,pages,query,queryData,route,runtimeMeta,settings&__device=phone`;
-
-  const res = await fetch(url, {
-    headers: {
-      "User-Agent": "Mozilla/5.0",
-      Accept: "application/json",
-    },
-  });
-
-  if (!res.ok) {
-    throw new Error(`Jumbo response error: ${res.status}`);
-  }
-
-  const json = await res.text();
-  const match = json.match(/\\"recordsFiltered\\":\s*(\d+)/i);
-
-  return match ? Number(match[1]) : 0;
-};
-
 export const getProductsJumbo = async (query) => {
-  const url = `https://www.jumbocolombia.com/${encodeURIComponent(
-    query
-  )}/s?_q=${encodeURIComponent(
-    query
-  )}&map=ft&__pickRuntime=appsEtag,blocks,blocksTree,components,contentMap,extensions,messages,page,pages,query,queryData,route,runtimeMeta,settings&__device=phone`;
+  const itemsPerPage = 28;
+  let currentPage = 0;
+  let totalPages = null;
+  let products = [];
 
-  const res = await fetch(url, {
-    headers: {
-      "user-agent": "Mozilla/5.0",
-      accept: "text/html",
-    },
-  });
+  do {
+    const from = currentPage * itemsPerPage;
+    const to = from + itemsPerPage - 1;
 
-  if (!res.ok) {
-    throw new Error(`Jumbo response error: ${res.status}`);
-  }
+    const variables = {
+      hideUnavailableItems: true,
+      skusFilter: "ALL_AVAILABLE",
+      simulationBehavior: "default",
+      installmentCriteria: "MAX_WITHOUT_INTEREST",
+      productOriginVtex: false,
+      map: "ft",
+      query,
+      orderBy: "OrderByScoreDESC",
+      from,
+      to,
+      selectedFacets: [{ key: "ft", value: query }],
+      fullText: query,
+      facetsBehavior: "Static",
+      categoryTreeBehavior: "default",
+      withFacets: false,
+    };
 
-  const json = await res.text();
+    const encodedVariables = btoa(JSON.stringify(variables));
 
-  // Extraemos los nombres y arreglamos formato
-  const names = [...json.matchAll(/\\"productName\\":\\"([^"]+)\\"/g)].map(
-    (m) => m[1]
-  );
+    const url = `https://www.jumbocolombia.com/_v/segment/graphql/v1?workspace=master&maxAge=short&appsEtag=remove&domain=store&locale=es-CO&operationName=productSearchV3&extensions=${encodeURIComponent(
+      JSON.stringify({
+        persistedQuery: {
+          version: 1,
+          sha256Hash:
+            "31d3fa494df1fc41efef6d16dd96a96e6911b8aed7a037868699a1f3f4d365de",
+          sender: "vtex.store-resources@0.x",
+          provider: "vtex.search-graphql@0.x",
+        },
+        variables: encodedVariables,
+      })
+    )}`;
 
-  // Extraemos precios
-  const prices = [
-    ...json.matchAll(/\\"TotalValuePlusInterestRate\\":\s*(\d+)/g),
-  ].map((m) => Number(m[1]));
+    const res = await fetch(url, {
+      headers: {
+        "user-agent": "Mozilla/5.0",
+        accept: "text/html",
+      },
+    });
 
-  // Calculamos el peso
-  const pricePerUnitList = names.map((name, i) => {
-    let qty = 1;
-    const match = name.match(/(?:x)?(\d+(?:\.\d+)?)\s*(g|kg)/i);
-    if (match) {
-      qty = Number(match[1]);
-      const unit = match[2].toLowerCase();
-      // Convertimos kg a gramos
-      if (unit === "kg") {
-        qty *= 1000;
-      }
+    if (!res.ok) {
+      throw new Error(`Jumbo response error: ${res.status}`);
     }
-    return qty !== 1 && prices[i] ? Number((prices[i] / qty).toFixed(3)) : 1;
-  });
 
-  // Extraemos sku de cada producto y creamos links
-  const skus = [...json.matchAll(/\\"linkText\\":\\"([^"]+)\\"/g)].map(
-    (m) => m[1]
-  );
-  const links = skus.map((sku) => `https://www.jumbocolombia.com/${sku}/p`);
+    const json = await res.text();
 
-  // Evitamos error de pricePerUnit indefinido
-  const products = names.map((name, i) => ({
-    name,
-    price: prices[i] || 0,
-    pricePerUnit: pricePerUnitList[i],
-    link: links[i],
-  }));
+    // Cuando es la primera iteracion, calculamos el total de paginas
+    if (totalPages === null) {
+      const match = json.match(/"recordsFiltered":\s*(\d+)/i);
+      if (!match) break;
+
+      const itemsFound = Number(match[1]);
+      totalPages = Math.ceil(itemsFound / itemsPerPage);
+    }
+
+    // Extraemos los nombres y arreglamos formato
+    const names = [...json.matchAll(/"productName":"([^"]+)"/g)].map(
+      (m) => m[1]
+    );
+
+    // Extraemos precios
+    const prices = [
+      ...json.matchAll(/"TotalValuePlusInterestRate":\s*(\d+)/g),
+    ].map((m) => Number(m[1]));
+
+    // Calculamos el peso
+    const pricePerUnit = names.map((name, i) => {
+      let qty = 1;
+      const match = name.match(/(?:x)?(\d+(?:\.\d+)?)\s*(g|kg)/i);
+      if (match) {
+        qty = Number(match[1]);
+        const unit = match[2].toLowerCase();
+        // Convertimos kg a gramos
+        if (unit === "kg") {
+          qty *= 1000;
+        }
+      }
+      return qty !== 1 && prices[i] ? Number((prices[i] / qty).toFixed(3)) : 1;
+    });
+
+    // Extraemos sku de cada producto y creamos links
+    const skus = [...json.matchAll(/"linkText":"([^"]+)"/g)].map((m) => m[1]);
+    const links = skus.map((sku) => `https://www.jumbocolombia.com/${sku}/p`);
+
+    names.forEach((name, i) => {
+      products.push({
+        name,
+        price: prices[i],
+        pricePerUnit: pricePerUnit[i],
+        link: links[i],
+      });
+    });
+
+    currentPage++;
+  } while (currentPage <= totalPages);
 
   return products;
 };
