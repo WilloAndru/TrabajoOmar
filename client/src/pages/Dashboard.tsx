f42215cbe703a1951c0d30090c500de7c59eaf9c
Wilson <wilsonandrescriollo@gmail.com>
2026-06-04 11:20:17 -0500

    Error no usestate usado

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { useState, useMemo } from "react";
+import { useMemo } from "react";
 import { useSearch } from "../hooks/useSearch";
 import { Link, Navigate, useLocation } from "react-router-dom";
 
